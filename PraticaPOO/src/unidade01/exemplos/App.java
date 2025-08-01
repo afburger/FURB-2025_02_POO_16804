f28200dc896df4d58048d975e63bacdaa362256c
André <andre@iburger.com.br>
2025-07-31 21:16:20 -0300

    feat: Exemplo 01

---
+++ PraticaPOO/src/unidade01/exemplos/App.java
@@ -0,0 +1,42 @@
+package unidade01.exemplos;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // DECLARAÇÃO DO OBJETO
+        Cachorro dog1;
+
+        // CRIAÇÃO DO OBJETO
+        dog1 = new Cachorro();
+
+        // UTILIZAÇÃO DO OBJETO (ATRIBUINDO VALORES)
+        dog1.nome = "Pingo";
+        dog1.raca = "Labrador";
+        dog1.cor = "Branco";
+        dog1.peso = 12.0;
+
+        // DECLARAÇÃO DO OBJETO E CRIAÇÃO DO OBJETO
+        Cachorro dog2 = new Cachorro();
+
+        dog2.nome = "Brutus";
+        dog2.raca = "Pastor alemão";
+        dog2.cor = "Marrom";
+        dog2.peso = 21.0;
+
+        // UTILIZAÇÃO DO OBJETO (LENDO OS VALORES)
+        System.out.println("Informações do dog 1");
+        System.out.println("Nome: " + dog1.nome);
+        System.out.println("Raça: " + dog1.raca);
+        System.out.println("Cor: " + dog1.cor);
+        System.out.println("Peso: " + dog1.peso);
+
+        // UTILIZAÇÃO DO OBJETO (ALTERANDO O ESTADO/VALOR)
+        dog1.peso = dog1.peso + 2;
+
+        System.out.println("Nome: " + dog1.nome);
+        System.out.println("Peso: " + dog1.peso);
+    }
+
+
+}
---
+++ PraticaPOO/src/unidade01/exemplos/Cachorro.java
@@ -0,0 +1,24 @@
+package unidade01.exemplos;
+
+/**
+ * Exemplo de DECLARAÇÃO DE CLASSE
+ */
+public class Cachorro {
+
+    String nome;
+    String raca;
+    String cor;
+    double peso;
+
+    void latir() {
+        System.out.println("AU AU!");
+    }
+
+    void abanarRabo() {
+       System.out.println("Abanando o rabo.");
+    }
+
+    void pegarCoisa(String coisa) {
+        System.out.println("Pegando " + coisa);
+    }
+}
