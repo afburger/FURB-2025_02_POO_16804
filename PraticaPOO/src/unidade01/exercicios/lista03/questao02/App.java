30a98830cf18e48d80a83e77454f7f6daac5f19c
André Felipe Bürger <andre.burger@publicatecnologia.com.br>
2025-08-27 14:18:10 -0300

    feat: Correção Lista Exercícios 03

---
+++ PraticaPOO/src/unidade01/exercicios/lista03/questao02/App.java
@@ -0,0 +1,41 @@
+package unidade01.exercicios.lista03.questao02;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Informe quantos livros deseja cadastrar");
+		int quantidadeLivros = teclado.nextInt();
+
+		Livro[] livros = new Livro[quantidadeLivros];
+
+		for (int i = 0; i < livros.length; i++) {
+
+			System.out.println("Informe o título do livro N." + i);
+			String titulo = teclado.next();
+
+			System.out.println("Informe o autor do livro N." + i);
+			String autor = teclado.next();
+
+			System.out.println("Informe o ano de publicação do livro N." + i);
+			int anoPublicacao = teclado.nextInt();
+
+			livros[i] = new Livro(titulo, autor, anoPublicacao);
+		}
+
+		for (int i = 0; i < livros.length; i++) {
+			Livro livroCadastrado = livros[i];
+			if (i % 2 == 0) {
+				System.out.println(livroCadastrado.exibirInfo(true));
+			} else {
+				System.out.println(livroCadastrado.exibirInfo());
+			}
+
+		}
+
+		System.out.println("Total de livros cadastrados: " + Livro.getTotalLivros());
+	}
+}
---
+++ PraticaPOO/src/unidade01/exercicios/lista03/questao02/Livro.java
@@ -0,0 +1,37 @@
+package unidade01.exercicios.lista03.questao02;
+
+public class Livro {
+
+	private String titulo;
+	private String autor;
+	private int anoPublicacao;
+	private static int totalLivros;
+
+	public Livro() {
+		this("Sem título", "Desonhecido", 0);
+	}
+
+	public Livro(String titulo, String autor, int ano) {
+		this.titulo = titulo;
+		this.autor = autor;
+		anoPublicacao = ano;
+		totalLivros++;
+	}
+
+	public String exibirInfo() {
+		return "Título: " + titulo
+				+ " Autor: " + autor
+				+ " Ano de publicação: " + anoPublicacao;
+	}
+
+	public String exibirInfo(boolean resumido) {
+		if (resumido) {
+			return "Título: " + titulo;
+		}
+		return exibirInfo();
+	}
+
+	public static int getTotalLivros() {
+		return totalLivros;
+	}
+}
