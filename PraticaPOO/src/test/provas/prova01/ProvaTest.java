3265fd34c33a50b43248c7286d4b3401e100601e
André <andre@iburger.com.br>
2025-10-03 23:05:16 -0300

    feat: Correção Prova 01

--- PraticaPOO/src/test/provas/prova01/ProvaTest.java
+++ PraticaPOO/src/test/provas/prova01/ProvaTest.java
@@ -1,49 +1,122 @@
 package provas.prova01;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import provas.prova01.correcao.Consulta;
-import provas.prova01.correcao.Hospital;
-import provas.prova01.correcao.Medico;
-import provas.prova01.correcao.Paciente;
+import org.junit.jupiter.api.TestMethodOrder;
+import provas.prova01.correcao.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ProvaTest {
 
+	/**
+	 * 0,25
+	 */
 	@Test
-	void testarMetodosHospital() {
+	@Order(1)
+	void testClassePaciente() {
+		Paciente paciente = new Paciente("Nome", "CPF", 18);
+		assertNotNull(paciente.getNome());
+		assertNotNull(paciente.getCpf());
+		assertEquals(18, paciente.getIdade());
+		assertNotNull(paciente.exibirDados());
+	}
+
+	/**
+	 * 0,25
+	 */
+	@Test
+	@Order(2)
+	void testClasseMedico() {
+		Medico medico = new Medico("NOME", "CRM");
+		assertNotNull(medico.getNome());
+		assertNotNull(medico.getCrm());
+		assertNotNull(medico.exibirDados());
+	}
+
+	/**
+	 * 0,25
+	 */
+	@Test
+	@Order(3)
+	void testClasseEndereco() {
+		Endereco endereco = new Endereco("Antonio da Veiga", 140, "Blumenau");
+		assertNotNull(endereco.getRua());
+		assertEquals(140, endereco.getNumero());
+		assertNotNull(endereco.getCidade());
+	}
+
+	/**
+	 * 0,25
+	 */
+	@Test
+	@Order(4)
+	void testClasseConsulta() {
+		Paciente paciente = new Paciente("Nome", "CPF", 18);
+		Medico medico = new Medico("NOME", "CRM");
+
+		Consulta consulta = new Consulta(paciente, medico, LocalDate.now());
+		consulta.adicionarObservacao("Teste");
+
+		assertNotNull(consulta.exibirDados());
+
+	}
+
+	/**
+	 * 1,00
+	 */
+	@Test
+	@Order(5)
+	void testClasseHospital() {
+		Paciente paciente = new Paciente("Nome", "CPF", 18);
+		Medico medico = new Medico("NOME", "CRM");
+		Endereco endereco = new Endereco("Antonio da Veiga", 140, "Blumenau");
+
 		Hospital hospital = new Hospital();
-		Paciente p = new Paciente("Nome", "123456789", 25);
-		Medico m = new Medico("Medico", "12345");
-		LocalDate dataConsulta = LocalDate.of(2025, 9, 20);
+		hospital.setEndereco(endereco);
 
-		Consulta consulta = hospital.agendarConsulta(p, m, dataConsulta);
-		Assertions.assertNotNull(consulta);
-
+		Consulta consulta = hospital.agendarConsulta(paciente, medico, LocalDate.now());
+		assertNotNull(consulta);
+		hospital.listarConsultas();
 		hospital.cancelarConsulta(consulta);
 
-		ArrayList<Consulta> consultas = hospital.getConsultas();
-		Assertions.assertNotNull(consultas);
-
-		ArrayList<Medico> medicos = hospital.getMedicos();
-		Assertions.assertNotNull(medicos);
-
-		ArrayList<Paciente> pacientes = hospital.getPacientes();
-		Assertions.assertNotNull(pacientes);
+		assertNotNull(hospital.getConsultas());
+		assertNotNull(hospital.getMedicos());
+		assertNotNull(hospital.getPacientes());
 	}
 
+	/**
+	 * 0,50
+	 */
 	@Test
-	public void testarMetodosConsulta() {
-		Paciente p = new Paciente("Nome", "123456789", 25);
-		Medico m = new Medico("Medico", "12345");
-		LocalDate d = LocalDate.of(2025, 9, 20);
+	@Order(6)
+	void testMetodosObrigatorios() {
+		Paciente paciente = new Paciente("Nome", "CPF", 18);
+		Medico medico = new Medico("NOME", "CRM");
 
-		Consulta consulta = new Consulta(p, m, d);
-		Assertions.assertNotNull(consulta);
+		Hospital hospital = new Hospital();
 
-		consulta.adicionarObservacao("Teste");
-		Assertions.assertNotNull(consulta.exibirDados());
+		Consulta consulta = hospital.agendarConsulta(paciente, medico, LocalDate.now());
+		assertNotNull(consulta);
+
+		hospital.listarConsultas();
+
+		consulta.adicionarObservacao("Obs");
 	}
+
+	/**
+	 * 0,50
+	 */
+	@Test
+	@Order(7)
+	void testValidacoesObrigatorias() {
+		assertThrows(IllegalArgumentException.class, () -> new Medico("NOME", null));
+		assertThrows(IllegalArgumentException.class, () -> new Paciente("NOME", null, 18));
+		assertThrows(IllegalArgumentException.class, () -> new Paciente("NOME", "CPF", -5));
+	}
+
 }
