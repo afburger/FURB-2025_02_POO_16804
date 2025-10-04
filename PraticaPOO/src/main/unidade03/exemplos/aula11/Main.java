9571ff8339dcfb8b9173caf9e26cb9987dd47692
André <andre@iburger.com.br>
2025-10-03 23:06:19 -0300

    feat: Atualiza Exemplos Unidade 03

--- PraticaPOO/src/main/unidade03/exemplos/aula11/Main.java
+++ PraticaPOO/src/main/unidade03/exemplos/aula11/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		Retangulo ret = new Retangulo();
-		ret.setAltura(10);
-		ret.setLargura(5);
+		ret.setLadoA(10);
+		ret.setLadoB(5);
 		ret.desenhar();
 
 		Figura estrla = new Figura() {
